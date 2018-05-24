c4092bc7db285cbc8524932952be766b16cbf3bf
Keita <koron0902@gmail.com>
2018-05-25 00:32:49 +0900

    [fix]Sanitizerに蹴られたのでテストの実装法を変更

--- test/util/math/crc16.cpp
+++ test/util/math/crc16.cpp
@@ -6,7 +6,8 @@
 BOOST_AUTO_TEST_SUITE(crc16)
 
 BOOST_AUTO_TEST_CASE(crc16_ibm){
-	uint16_t crc = ai::util::math::crc16({0x55, 0x44});
+	std::vector<uint8_t> frame{0x55, 0x44};
+	uint16_t crc = ai::util::math::crc16(frame);
 	BOOST_TEST(crc == 0xFF90);
 }
 
