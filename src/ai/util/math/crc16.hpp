d57cd5bb4849c4ef37fe2b82d23a8754ff40a524
Keita <koron0902@gmail.com>
2018-05-24 23:22:11 +0900

    [text]コメントの整理

--- src/ai/util/math/crc16.hpp
+++ src/ai/util/math/crc16.hpp
@@ -8,6 +8,9 @@
 namespace ai{
 	namespace util{
 		namespace math{
+			/// @brief CRC16を計算する
+			/// @param _buf CRC計算対象のデータを格納したコンテナ
+			/// @return CRC計算結果
 			uint16_t crc16(const std::vector<uint8_t>& _buf);
 		}
 	}
