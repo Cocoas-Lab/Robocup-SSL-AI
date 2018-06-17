bce136e5027d43a4fa990217cabc741bb8c36030
Keita <koron0902@gmail.com>
2018-06-17 22:40:41 +0900

    [add]robotの情報を格納するmodelを作成した

---
+++ src/ai/model/robot.hpp
@@ -0,0 +1,47 @@
+#ifndef AI_MODEL_ROBOT_HPP_
+#define AI_MODEL_ROBOT_HPP_
+
+#include <stdint.h>
+
+namespace ai{
+	namespace model{
+		class robot{
+			uint32_t id_;
+			double x_;
+			double y_;
+			double vx_;
+			double vy_;
+			double ax_;
+			double ay_;
+			double theta_;
+			double omega_;
+
+		public:
+			robot();
+			robot(uint32_t _id, double _x, double _y);
+			robot(const robot&) = default;
+
+			uint32_t id();
+			double x();
+			double y();
+			double vx();
+			double vy();
+			double ax();
+			double ay();
+			double theta();
+			double omega();
+
+			void id(uint32_t _id);
+			void x(double _x);
+			void y(double _y);
+			void vx(double _vx);
+			void vy(double _vy);
+			void ax(double _ax);
+			void ay(double _ay);
+			void theta(double _theta);
+			void omega(double _omega);
+		};
+	}
+}
+
+#endif
