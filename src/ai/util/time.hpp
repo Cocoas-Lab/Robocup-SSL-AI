cb1d6ab852f9895456242ea2861639265b4a4fe4
Keita <koron0902@gmail.com>
2018-06-21 17:15:56 -0400

    [fix]型のタイプの修正と関数名の修正

--- src/ai/util/time.hpp
+++ src/ai/util/time.hpp
@@ -1,7 +1,7 @@
 #ifndef AI_SERVER_UTIL_TIME_HPP_
 #define AI_SERVER_UTIL_TIME_HPP_
 
-#include "detail/time.hpp"
+#include "decision/time.hpp"
 
 namespace ai {
 namespace util {
@@ -10,10 +10,10 @@ namespace util {
 using ClockType = std::chrono::high_resolution_clock;
 
 /// プロジェクト内で用いる時間を表現する型
-using DurationType = typename clock_type::duration;
+using DurationType = typename ClockType::duration;
 
 /// プロジェクト内で用いる時刻を表現する型
-using TimePointType = typename clock_type::time_point;
+using TimePointType = typename ClockType::time_point;
 
 /// @brief        浮動小数点数で表現された時間[s]をstd::chrono::durationに変換する
 /// @param time   変換する時間
@@ -22,8 +22,8 @@ using TimePointType = typename clock_type::time_point;
 /// VisionやRefBoxがこのような形式で時間を送ってくるので, それを変換するために実装.
 /// Durationを明示的に指定しなかった場合, microsecオーダでの変換を行う.
 template <class Duration = std::chrono::microseconds,
-          std::enable_if_t<detail::isDurationValue<Duration>, std::nullptr_t> = nullptr>
-inline constexpr Duration to_duration(double time) {
+          std::enable_if_t<decision::isDurationValue<Duration>, std::nullptr_t> = nullptr>
+inline constexpr Duration toDuration(double time) {
   using rep    = typename Duration::rep;
   using period = typename Duration::period;
   return Duration{static_cast<rep>((time * period::den) / period::num)};
