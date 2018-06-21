c56be6e69cd753baa37a1fe3c902c8f61742dbfa
Keita <koron0902@gmail.com>
2018-06-21 16:40:07 -0400

    [add]与えられた型が`std::chrono::duration`かどうかを判定する関数を作成

---
+++ src/ai/util/decision/time.hpp
@@ -0,0 +1,25 @@
+#ifndef AI_SERVER_UTIL_DECISION_TIME_HPP_
+#define AI_SERVER_UTIL_DECISION_TIME_HPP_
+
+#include <chrono>
+#include <type_traits>
+
+namespace ai {
+namespace util {
+namespace decision {
+
+// template型引数に与えた型がstd::chrono::durationかを判定するメタ関数
+template <class>
+struct isDuration : std::false_type {};
+
+template <class Rep, class Period>
+struct isDuration<std::chrono::duration<Rep, Period>> : std::true_type {};
+
+template <class T>
+static constexpr auto isDurationValue = isDuration<T>::value;
+
+} // namespace decision
+} // namespace util
+} // namespace ai_server
+
+#endif // AI_SERVER_UTIL_DETAIL_TIME_H
