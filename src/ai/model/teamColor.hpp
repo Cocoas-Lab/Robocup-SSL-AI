4dd058d50c04e70dcc8fa9ae410d25e64ebf573c
Keita <koron0902@gmail.com>
2018-06-24 17:40:16 -0400

    [clean]コード整形した

--- src/ai/model/teamColor.hpp
+++ src/ai/model/teamColor.hpp
@@ -1,13 +1,13 @@
 #ifndef AI_MODEL_TEAM_COLOR_HPP_
 #define AI_MODEL_TEAM_COLOR_HPP_
 
-namespace ai{
-namespace model{
-enum teamColor{
-	Blue,
-	Yellow,
+namespace ai {
+namespace model {
+enum teamColor {
+  Blue,
+  Yellow,
 };
 }
-}
+} // namespace ai
 
 #endif
