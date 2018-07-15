fa8fd6f57c314a4d94b4a3c690f2b9f74b585506
Keita <koron0902@gmail.com>
2018-07-16 00:12:36 +0900

    [add]field情報を格納するためのclassを作成した

---
+++ src/ai/model/field.cpp
@@ -0,0 +1,71 @@
+#include "field.hpp"
+
+namespace ai{
+namespace model{
+  field::field():length_(12000),width_(9000),centerRadius_(500),goalWidth_(1200),penaltyLength_(1200),penaltyWidth_(2400){}
+
+  uint32_t field::length() const{
+	return length_;
+  }
+
+  uint32_t field::width() const{
+	return width_;
+  }
+
+  uint32_t field::centerRadius() const{
+	return centerRadius_;
+  }
+
+  uint32_t field::goalWidth() const{
+	return goalWidth_;
+  }
+
+  uint32_t field::penaltyLength() const{
+	return penaltyLength_;
+  }
+
+  uint32_t field::penaltyWidth() const{
+	return penaltyWidth_;
+  }
+
+  void field::length(uint32_t _length){
+	length_ = _length;
+  }
+
+  void field::width(uint32_t _width){
+	width_ = _width;
+  }
+
+  void field::centerRadius(uint32_t _centerRadius){
+	centerRadius_ = _centerRadius;
+  }
+
+  void field::goalWidth(uint32_t _goalWidth){
+	goalWidth_ = _goalWidth;
+  }
+
+  void field::penaltyLength(uint32_t _penaltyLength){
+	penaltyLength_ = _penaltyLength;
+  }
+
+  void field::penaltyWidth(uint32_t _penaltyWidth){
+	penaltyWidth_ = _penaltyWidth;
+  }
+
+  double field::xMax() const{
+	return (length_ / 2);
+  }
+
+  double field::xMin() const{
+	return -(length_ / 2);
+  }
+
+  double field::yMax() const{
+	return (width_ / 2);
+  }
+
+  double field::yMin() const{
+	return -(width_ / 2);
+  }
+}
+}
---
+++ src/ai/model/field.hpp
@@ -0,0 +1,42 @@
+#ifndef AI_MODEL_FIELD_HPP_
+#define AI_MODEL_FIELD_HPP_
+
+#include <stdint.h>
+
+namespace ai{
+namespace model{
+class field{
+private:
+  uint32_t length_;
+  uint32_t width_;
+  uint32_t centerRadius_;
+  uint32_t goalWidth_;
+  uint32_t penaltyLength_;
+  uint32_t penaltyWidth_;
+
+public:
+  field();
+
+  uint32_t length() const;
+  uint32_t width() const;
+  uint32_t centerRadius() const;
+  uint32_t goalWidth() const;
+  uint32_t penaltyLength() const;
+  uint32_t penaltyWidth() const;
+
+  void length(uint32_t _length);
+  void width(uint32_t _width);
+  void centerRadius(uint32_t _centerRadius);
+  void goalWidth(uint32_t _goalWidth);
+  void penaltyLength(uint32_t _penaltyLength);
+  void penaltyWidth(uint32_t _penaltyWidth);
+
+  double xMax() const;
+  double xMin() const;
+  double yMax() const;
+  double yMin() const;
+};
+}
+}
+
+#endif
