b638f90575efa29d8fdf447ddf0a6cb7152f9ec2
Keita <koron0902@gmail.com>
2018-07-16 13:58:13 +0900

    [fix]eunmのキーが命名規則に反していた

---
+++ src/ai/model/command.cpp
@@ -0,0 +1,48 @@
+#include "command.hpp"
+
+namespace ai {
+namespace model {
+
+command::command(uint32_t _id)
+    : id_(_id),
+      dribble_(0),
+      kick_(command::kickType::None, 0.0),
+      setpoint_(command::velocity{0.0, 0.0, 0.0}) {}
+
+uint32_t command::id() const {
+  return id_;
+}
+
+int32_t command::dribble() const {
+  return dribble_;
+}
+
+command::KickFlag command::kick() const {
+  return kick_;
+}
+
+const command::Setpoint& command::setpoint() const {
+  return setpoint_;
+}
+
+void command::dribble(int32_t _dribble) {
+  dribble_ = _dribble;
+}
+
+void command::kick(const command::KickFlag& _kick) {
+  kick_ = _kick;
+}
+
+void command::pos(const command::position& _position) {
+  setpoint_ = _position;
+}
+
+void command::vel(const command::velocity& _velocity) {
+  setpoint_ = _velocity;
+}
+
+void command::accel(const command::acceleration& _accel) {
+  setpoint_ = _accel;
+}
+} // namespace model
+} // namespace ai_server
---
+++ src/ai/model/command.hpp
@@ -0,0 +1,58 @@
+#ifndef AI_MODEL_COMMAND_HPP_
+#define AI_MODEL_COMMAND_HPP_
+
+#include <tuple>
+#include <variant>
+#include <stdint.h>
+
+namespace ai {
+namespace model {
+
+class command {
+public:
+  enum class kickType { None, Straight, Tip };
+
+  struct position {
+    double x;
+    double y;
+    double theta;
+  };
+
+  struct velocity {
+    double vx;
+    double vy;
+    double omega;
+  };
+
+  struct acceleration {
+    double ax;
+    double ay;
+    double alpha;
+  };
+
+  using Setpoint  = std::variant<position, velocity, acceleration>;
+  using KickFlag = std::tuple<kickType, double>;
+
+  explicit command(uint32_t _id);
+
+  uint32_t id() const;
+  int32_t dribble() const;
+  KickFlag kick() const;
+  const Setpoint& setpoint() const;
+
+  void dribble(int32_t _dribble);
+  void kick(const KickFlag& _kick);
+  void pos(const position& _position);
+  void vel(const velocity& _velocity);
+	void accel(const acceleration& _accel);
+
+private:
+  uint32_t id_;
+  int32_t dribble_;
+  KickFlag kick_;
+  Setpoint setpoint_;
+};
+} // namespace model
+} // namespace ai_server
+
+#endif
