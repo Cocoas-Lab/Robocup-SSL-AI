235382e76f3b1bf4c33fb786920baffefb010468
Keita <koron0902@gmail.com>
2018-07-26 02:08:55 +0900

    [fix]controllerのbaseクラスに速度制限の要素を追加

--- src/ai/controller/base.cpp
+++ src/ai/controller/base.cpp
@@ -2,6 +2,7 @@
 
 namespace ai {
 namespace controller {
+base::base(const double _limit) : velocityLimit_(_limit), stable_(false) {}
 
 velocity base::operator()(const model::robot& _robot, const position& _setpoint) {
   return update(_robot, _setpoint);
@@ -11,5 +12,20 @@ velocity base::operator()(const model::robot& _robot, const velocity& _setpoint)
   return update(_robot, _setpoint);
 }
 
+double base::velocityLimit() const {
+  return velocityLimit_;
+}
+
+void base::velocityLimit(const double _limit) {
+  velocityLimit_ = _limit;
+}
+
+bool base::stable() const {
+  return stable_;
+}
+
+void base::stable(const bool _stable) {
+  stable_ = _stable;
+}
 } // namespace controller
 } // namespace ai
--- src/ai/controller/base.hpp
+++ src/ai/controller/base.hpp
@@ -14,12 +14,20 @@ using velocity = model::command::velocity;
 
 class base : public boost::static_visitor<velocity> {
 public:
+  base(const double _limit);
   virtual ~base() = default;
 
   velocity operator()(const model::robot& _robot, const position& _setpoint);
   velocity operator()(const model::robot& _robot, const velocity& _setpoint);
 
+  double velocityLimit() const;
+  virtual void velocityLimit(const double _limit);
+  bool stable() const;
+  virtual void stable(const bool _stable);
+
 protected:
+  double velocityLimit_;
+  bool stable_;
   virtual velocity update(const model::robot& _robot, const position& _setpoint) = 0;
   virtual velocity update(const model::robot& _robot, const velocity& _setpoint) = 0;
 };
