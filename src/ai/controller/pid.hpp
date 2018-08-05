789e4d61fa30181176fa8daeca42e4d20e99c551
Keita <koron0902@gmail.com>
2018-08-06 05:47:14 +0900

    [fix]pidゲインをいじった

--- src/ai/controller/pid.hpp
+++ src/ai/controller/pid.hpp
@@ -10,18 +10,18 @@ class pid : public base {
 private:
   double cycle_;                                         // 制御周期
   model::robot robot_;                                   // ロボット
-  static constexpr double kp_[2]           = {2.0, 4.0}; // 比例ゲイン(xy,rotate)
-  static constexpr double ki_[2]           = {0.2, 0.2}; // 積分ゲイン(xy,rotate)
-  static constexpr double kd_[2]           = {0.0, 0.0}; // 微分ゲイン(xy,rotate)
+  static constexpr double kp_[2]           = {42.5, 8.0}; // 比例ゲイン(xy,rotate)
+  static constexpr double ki_[2]           = {10.5, 6.2}; // 積分ゲイン(xy,rotate)
+  static constexpr double kd_[2]           = {0.03, 0.01}; // 微分ゲイン(xy,rotate)
   static constexpr double maxVelocity_     = 5000.0;     // 最大速度
-  static constexpr double maxAcceleration_ = 2000.0;     // 最大加速度
-  static constexpr double minAcceleration_ = 100.0;      // 最小加速度
-  static constexpr double reachSpeed_      = 1500.0;     // 加速度上昇の速さ
+  static constexpr double maxAcceleration_ = 4000.0;     // 最大加速度
+  static constexpr double minAcceleration_ = 750.0;      // 最小加速度
+  static constexpr double reachSpeed_      = 250.0;     // 加速度上昇の速さ
   velocity up_[2]; // 操作量(比例,1フレーム前まで)
   velocity ui_[2]; // 操作量(積分,1フレーム前まで)
   velocity ud_[2]; // 操作量(微分,1フレーム前まで)
   velocity u_[2];  // 操作量(1フレーム前まで)
-  velocity e_[2];  // 偏差(1フレーム前まで)
+	velocity e_[2];  // 偏差(1フレーム前まで)
   // 入力制限
   void limitation();
 
