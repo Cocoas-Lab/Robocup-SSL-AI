715219d42b09926cc84def10fbc3a7a263cba656
Keita <koron0902@gmail.com>
2018-08-06 05:54:37 +0900

    [add]目標位置に動くためのactionとして`ai::game::action::move`を実装した

---
+++ src/ai/game/action/move.hpp
@@ -0,0 +1,29 @@
+#ifndef AI_GAME_ACTION_MOVE_H
+#define AI_GAME_ACTION_MOVE_H
+
+#include "base.hpp"
+#include "ai/model/command.hpp"
+#include <memory>
+
+namespace ai {
+namespace game {
+namespace action {
+
+class move : public base {
+public:
+  using base::base;
+
+  void moveTo(double _x, double _y, double _theta = 0.0);
+
+  model::command execute() override;
+
+private:
+  double x_;
+  double y_;
+  double theta_;
+};
+} // namespace action
+} // namespace game
+} // namespace ai_server
+
+#endif
