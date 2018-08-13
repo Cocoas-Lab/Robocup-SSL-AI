2d75e744dcd789293aa2c9045728afe0087ddad4
Keita <koron0902@gmail.com>
2018-08-13 15:35:45 +0900

    [add]`ai::game::action::getball`において、一定以上の速度を持ったボールに対して回り込むような処理を書いた(要実機検証)

--- src/ai/game/action/getBall.cpp
+++ src/ai/game/action/getBall.cpp
@@ -9,7 +9,6 @@
 #include "ai/util/math/angle.hpp"
 #include "ai/util/math/geometry.hpp"
 
-
 namespace ai {
 namespace game {
 namespace action {
@@ -119,6 +118,58 @@ model::command getBall::execute() {
       }
       theta = util::math::wrapToPi(std::atan2(target.y() - robot.y(), target.x() - robot.x()));
     }
+  } else {
+		// TODO:grSimでは検証が難しいので実機で検証する必要あり
+
+    // ボールが早いので予測位置に回り込む
+    // 内積で直交位置を導出
+    {
+      const auto normalized = ballVel.normalized();
+      const auto dist       = robot - ballPos;
+      const auto dot        = normalized.dot(dist);
+
+      position = (ballPos + dot * normalized);
+    }
+    command.kick({model::command::kickType::None, 0});
+
+    if (std::signbit(ballVel.dot(position - ballPos))) {
+      position = ball;
+      {
+        const auto margin = 400.0;
+        polygon poly;
+        const auto tmp          = util::math::calcIsoscelesVertexes(robot, position, margin);
+        bg::exterior_ring(poly) = boost::assign::list_of<point>(robot.x(), robot.y())(
+            std::get<0>(tmp).x(), std::get<0>(tmp).y())(
+            std::get<1>(tmp).x(), std::get<1>(tmp).y())(robot.x(), robot.y());
+        const point p(ballPos.x(), ballPos.y());
+
+        bool flag = false;
+        if ((ballPos - robot).norm() < 2000) flag = true;
+        //間にボールがあったら回り込む
+        if (!bg::disjoint(p, poly) && flag) {
+          //判定の幅
+          const auto margin       = 600.0;
+          const auto [tmp1, tmp2] = util::math::calcIsoscelesVertexes(robot, ballPos, margin);
+          const auto b2r          = std::atan2((robot - ball).y(), (robot - ball).x());
+          const auto b2p          = std::atan2((position - ball).y(), (position - ball).x());
+          position                = util::math::wrapToPi(b2p - b2r) < 0 ? tmp1 : tmp2;
+
+        } else {
+          const auto nb2r = std::atan2((robot - ballPos).y(), (robot - ballPos).x());
+          const auto nb2p = std::atan2((position - ball).y(), (position - ball).x());
+          // 当たらないようにする
+          if (std::abs(util::math::wrapToPi(nb2r - nb2p)) < 0.1) {
+            const auto margin = 200.0;
+            const auto [tmp1, tmp2] =
+                util::math::calcIsoscelesVertexes(robot, position, margin);
+            const auto b2r = std::atan2((robot - ball).y(), (robot - ball).x());
+            const auto b2p = std::atan2((position - ball).y(), (position - ball).x());
+            position       = util::math::wrapToPi(b2p - b2r) < 0 ? tmp1 : tmp2;
+          }
+        }
+      }
+    }
+    theta = util::math::wrapToPi(std::atan2(ballPos.y() - robot.y(), ballPos.x() - robot.x()));
   }
 
   std::vector<planner::rrt::obstacle> obstacles{};
