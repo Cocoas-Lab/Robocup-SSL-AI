3f9cf42d4d5d502419786afe31f7b28813a1e980
Keita <koron0902@gmail.com>
2018-08-17 12:37:29 +0900

    [text]コメントを追記した

--- src/ai/game/action/marking.hpp
+++ src/ai/game/action/marking.hpp
@@ -9,16 +9,30 @@ namespace game {
 namespace action {
 class marking : public base {
 public:
-  enum markMode { PassCut, ShootBlock };
+  enum markMode {
+    PassCut,   // ボールとロボットの間に入ってパスをカットする
+    ShootBlock // ロボットとゴールの間に入ってシュートをブロックする
+  };
 
   using base::base;
+
+  /// @brief マーキング対象を指定する
+  /// @param _id マーキング対象の敵ロボットID
+  /// @return none
   void mark(uint32_t _id);
+
+  /// @brief マーキングのモードを指定する
+  /// @param _mode マーキングのモード
+  /// @return none
   void mode(markMode _mode);
+
+  /// @brief アクションの処理
+  /// @return ロボットへの指令
   model::command execute() override;
 
 private:
-  uint32_t enemyId_;
-  markMode mode_;
+  uint32_t enemyId_; // マーキング対象のID
+  markMode mode_;    // マーキングのモード
 };
 } // namespace action
 } // namespace game
